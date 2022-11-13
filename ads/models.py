58d79140fd3b651458816cd71a8943c60d9b2021
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-11-13 12:20:05 +0500

    fix model

--- ads/models.py
+++ ads/models.py
@@ -1,21 +1,29 @@
 from django.db import models
+from users.models import User
 
 
-class Ad(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
-    name = models.CharField(max_length=150)
-    author = models.CharField(max_length=100)
-    price = models.PositiveIntegerField(default=0)
-    description = models.CharField(max_length=1000)
-    address = models.CharField(max_length=500)
-    is_published = models.BooleanField(default=None)
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+class Ad(models.Model):
+    name = models.CharField(max_length=150)
+    author = models.ForeignKey(User, verbose_name="author", on_delete=models.CASCADE, null=True)
+    price = models.PositiveIntegerField(default=0)
+    description = models.CharField(max_length=1000)
+    is_published = models.BooleanField(default=None)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
 
     def __str__(self):
         return self.name
--- users/models.py
+++ users/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from keyring.backends import null
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    lat = models.DecimalField(max_digits=8, decimal_places=6)
+    lng = models.DecimalField(max_digits=8, decimal_places=6)
+
+    class Meta:
+        verbose_name = "Местоположение"
+        verbose_name_plural = "Местоположения"
+
+    def __str__(self):
+        return self.name
+
+
+class UserRoles:
+    ADMIN = "admin"
+    MODERATOR = "moderator"
+    MEMBER = "member"
+    choice = (
+        (ADMIN, "Администратор"),
+        (MODERATOR, "Модератор"),
+        (MEMBER, "Пользователь"),
+    )
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Имя", null=True)
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия", null=True)
+    username = models.CharField(max_length=100, verbose_name="Логин", unique=True)
+    password = models.CharField(max_length=100, verbose_name="Пароль")
+    age = models.PositiveIntegerField()
+    location = models.ManyToManyField(Location)
+    roles = models.CharField(choices=UserRoles.choice, default=UserRoles.MEMBER, max_length=10)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return f'{self.first_name} ({self.last_name})'
